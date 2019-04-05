7ddefb49139cae639a2446a671e51bc4f7c7c2e6
Elizaveta Ustrikova <Gorodilova.E.Mi@sberbank.ru>
2019-04-05 13:38:05 +0300

    Slot finished

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -1,8 +1,47 @@
 package games;
 
+import static java.lang.Math.random;
+import static java.lang.StrictMath.round;
+
 public class Slot {
 
     public static void main(String... __) {
-        System.out.println("Hello, World!");
+        int initialRate = 100;
+        int rate = 10;
+        int prize = 1000;
+
+        int firstCounter = 0;
+        int secondCounter = 0;
+        int thirdCounter = 0;
+        int size = 7;
+
+        int gain = (int) Math.round(random() * 100);
+
+        while (initialRate > 0) {
+            int temp = initialRate;
+            initialRate -= rate;
+            firstCounter = (firstCounter + (int) round(random() * 100)) % size;
+            secondCounter = (secondCounter + (int) round(random() * 100)) % size;
+            thirdCounter = (thirdCounter + (int) round(random() * 100)) % size;
+            if (firstCounter == secondCounter && firstCounter == thirdCounter) {
+                temp += 1000;
+                System.out.printf("У Вас %d$, ставка - %d$\n"+
+                                "Крутим барабаны !Розыгрыш принёс следующие результаты:\n"+
+                                "первый барабан - %d, второй - %d, третий - %d\n"+
+                                "Выигрыш % d$, ваш капитал теперь составляет: %d$\n ",
+                        initialRate, rate, firstCounter, secondCounter, thirdCounter, rate, temp);
+            } else {
+                temp -= rate;
+                System.out.printf("У Вас %d$, ставка - %d$\n"+
+                        "Крутим барабаны !Розыгрыш принёс следующие результаты:\n"+
+                        "первый барабан - %d, второй - %d, третий - %d\n"+
+                        "Проигрыш % d$, ваш капитал теперь составляет: %d$\n ",
+                        initialRate, rate, firstCounter, secondCounter, thirdCounter, rate, temp);
+            }
+            initialRate = temp;
+            firstCounter = secondCounter = thirdCounter = 0;
+        }
+
+
     }
 }
